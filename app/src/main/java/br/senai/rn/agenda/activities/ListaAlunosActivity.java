355ec9e654b5a78b9a04ba21559c0918613112b4
Miguel Barbosa <miguelbarbosa1309@gmail.com>
2019-05-24 15:02:03 -0300

    mudei 24/05

--- app/src/main/java/br/senai/rn/agenda/activities/ListaAlunosActivity.java
+++ app/src/main/java/br/senai/rn/agenda/activities/ListaAlunosActivity.java
@@ -6,11 +6,18 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.senai.rn.agenda.dao.AlunoDAO;
 import br.senai.rn.agenda.models.Aluno;
@@ -45,14 +52,56 @@ public class ListaAlunosActivity extends AppCompatActivity {
         listaAlunos = findViewById(R.id.principal_activity_main_lista_de_alunos);
         botaoAdicionar = findViewById(R.id.principal_activity_main_fab_novo_aluno);
 
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-        listaAlunos.setAdapter(adapter);
+        configurarAdapter(listaAlunos);
 
         registerForContextMenu(listaAlunos);
 
         dao.salvar(new Aluno("Miguel", "998998876", "adsfkafdja@gmail.com"));
         dao.salvar(new Aluno("Marcos", "3453535", "sla@gmail.com"));
         dao.salvar(new Aluno("Wesley", "35353535", "isso@gmail.com"));
+    }
+
+    private void configurarAdapter(ListView listView) {
+        adapter = new ArrayAdapter<>(this, R.layout.item_aluno);
+
+        listView.setAdapter (new BaseAdapter() {
+
+            final List<Aluno> alunos = new ArrayList<>();
+
+            @Override
+            public int getCount() {
+                return alunos.size();
+            }
+
+            @Override
+            public Aluno getItem(int position) {
+                return alunos.get(position);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return alunos.get(position).getId();
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                Aluno aluno = getItem(position);
+
+                TextView nome = findViewById(R.id.item_aluno_nome);
+                TextView telefone = findViewById(R.id.item_aluno_telefone);
+
+                nome.setText(aluno.getNome());
+                telefone.setText(aluno.getTelefone());
+
+                View viewCriada = LayoutInflater
+                        .from(ListaAlunosActivity.this)
+                        .inflate(R.layout.item_aluno, parent);
+
+                return viewCriada;
+
+            }
+        });
     }
 
     private void definirEventos() {
